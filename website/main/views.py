b94cc08a08e259160fadd2b330a6d64b291ffd48
Евгений <Joker@yandex.ru>
2023-04-20 22:54:47 +0300

    news text

--- website/main/views.py
+++ website/main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'main/index.html')
+    date = {'title': 'Главная страница',
+            }
+    return render(request, 'main/index.html', date)
+
 
 def about(request):
     return render(request, 'main/about.html')
